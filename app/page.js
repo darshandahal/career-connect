cea5d102c5ef380cfdbc1f8d78a6d8e4a2edb171
darshan <darshandahal304@gmail.com>
2024-12-08 00:12:59 -0700

    simple commit

--- app/page.js
+++ app/page.js
@@ -5,7 +5,8 @@ export default function page() {
       <h3>Bridging the gap between opportunities and talent</h3>
       <p>CareerConnect is a React-based web application designed to simplify the job search process.
         It provides job seekers with an intuitive platform to browse job listings, filter opportunities based on location, industry, and role, and apply directly through the app.
-        Employers can post jobs and manage applications, creating a seamless connection between job seekers and employers.</p>
+        Employers can post jobs and manage applications, creating a seamless connection between job seekers and employers.
+        this is my first project </p>
     </div>
   )
 }
